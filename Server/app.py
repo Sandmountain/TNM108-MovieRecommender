33b5434877b59f409eb6f3e22836643fed09c60b
Sandmountain <sandtain@gmail.com>
2019-12-13 11:30:28 +0100

    Functional Recomendation system, returning a json object of top 8 movies

--- Server/app.py
+++ Server/app.py
@@ -1,13 +1,13 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 import requests
+import json
 from flask_cors import CORS, cross_origin
-from src.learn import getRandomMovie
+from src.learn import getRandomMovie, getRecomendation,getManyRecomendations
 
 app = Flask(__name__, static_url_path="")
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
 movieList = ''
 movieListArray = []
 movieFeatures = []
@@ -23,21 +23,24 @@ def add_movie():
     if not request.json or not 'title' in request.json:
         abort(400)
 
-    movieListArray.append(request.json['title'])
+    #movieListArray.append(request.json['title'])
     movieFeatures.append(getRecomendation(request.json['title'])[0])
-
-    print(movieFeatures)
-    return "hello", 201
-
+    movieListArray = getManyRecomendations(movieFeatures)
+    response = app.response_class(
+        response=json.dumps(movieListArray),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+   
 
 # Gets the movies depending on current user
 @app.route('/movies')
 @cross_origin()
 def movies():
     #res = requests.get('https://jsonplaceholder.typicode.com/users')
+
     newMovie = getRandomMovie()
-    # if len(res.content) == 0:
-    #     abort(404)
     return newMovie, 201
 
 
--- Server/src/learn.py
+++ Server/src/learn.py
@@ -13,12 +13,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 def get_title_from_index(index):
     return df["original_title"].iloc[index]
 
-
 def get_index_from_title(title):
-    #print(title)
-    #print(df[df["original_title"] == title][])
-    return df[df.original_title == title].index.values.astype(int)[0]
+    return df[df["original_title"] == title].index[0]
 
+def get_poster_path_from_index(index):
+    return df["poster_path"].iloc[index]
 
 def combine_features(row):
     try:
@@ -51,12 +50,87 @@ def init():
 def getRecomendation(movie_user_likes):  
     # Step 6: Get index of this movie from its title
     movie_index = get_index_from_title(movie_user_likes)
-    return df["combined_features"][movie_index], getRecomendationList(cSimilarity[movie_index])
+    print(cSimilarity[movie_index])
+    return movie_index, getRecomendationList(cSimilarity[movie_index])
+
+def getManyRecomendations(movie_indexes):
+    #Loop through all indexes
+    similar_movies_array = []
+    #if using stars: for info in movie_raitings => info.index
+    for index in movie_indexes:
+        similar_movies_array.append(getRecomendationList(cSimilarity[index]))
+
+    #print(similar_movies_array)
+    return chooseMovies(similar_movies_array, movie_indexes)
+
+#importing the list, and/or the raiting
+def chooseMovies(similar_movie_list, movie_indexes):
+    #A list of similiarlities to other movies, sorted with highest similiariteis first.
+    #if weighting: 
+    #(star: 1-2, listOfSimiliarMovies), if len(movie_indexes) < 2, resort and set reverse=False
+    #(star: 3, listOfSimiliarMovies), if len(movie_indexes) < 2, use similar_movie_list[len(similar_movie_list)/2][:10]
+    #(star: 4-5, listOfSimiliarMovies), return top 10 or w/e
+    moviesToRecommend = set()
+    #design choise: choosing 4 movies from the latest added, 4 old ones.
+    if(len(movie_indexes) == 1):
+        for i in range(1,9):
+            moviesToRecommend.add((get_title_from_index(similar_movie_list[0][i][0])))
+    else:
+    # Take 4 random movies from older ones.
+        moviesToRecommend = set()
+        for i in range(1, 5):
+            #take a random movie then select a random of the 5 most similar in the lists
+            #sizeOfSet = len(moviesToRecommend)
+            sizeOfSet = len(moviesToRecommend)
+            while(True):
+                randMovie = random.randint(0, len(movie_indexes)-1)
+                randSimilar = random.randint(1, 5)
+                moviesToRecommend.add(get_title_from_index(similar_movie_list[randMovie][randSimilar][0]))
+                if(sizeOfSet < len(moviesToRecommend)):
+                    break
+
+        for i in range(1, 5):   
+            sizeOfSet = len(moviesToRecommend)
+            counter = 0
+            while(True):
+                randMovie = random.randint(0, len(movie_indexes)-1)
+                counter = counter + 1
+                moviesToRecommend.add(get_title_from_index(similar_movie_list[-1][counter][0]))
+                if(sizeOfSet < len(moviesToRecommend)):
+                    break            
+
+    return formatMovieList(list(moviesToRecommend))
+    
+def formatMovieList(moviesToRecommend):
+    toReturn = []
+    print(moviesToRecommend)
+    for movie in moviesToRecommend:
+        movieIndex = get_index_from_title(movie)
+        toReturn.append(
+            {
+                "id": str(movieIndex),
+                "title": movie,
+                "path": get_poster_path_from_index(movieIndex)
+            }
+        )
+    return toReturn
+
+
+"""
+def getUniqueMovie(movie_recommendation_list, movie, listLength, movieList, movieIndex, counter):
+    if(movie in movie_recommendation_list):
+        # Fix randSimilar to increment.
+        # Väljer någon av de 4 första filmerna.
+        counter = counter + 1
+        getUniqueMovie(movie_recommendation_list, get_title_from_index(movieList[movieIndex][counter][0]), listLength, movieList, movieIndex, counter)
+    else:
+        print(movie_recommendation_list)
+        print(movie)
+        return movie
+"""
 
 def getRecomendationList(cosineMatrix):
-    
     similar_movies = list(enumerate(cosineMatrix))
-
     # Step 7: Get a list of similar movies in descending order of similarity score
     return sorted(similar_movies, key=lambda x: x[1], reverse=True)
 
@@ -85,10 +159,6 @@ def getRandomMovie():
 # Step 1: Read CSV File
 df = pd.read_csv("./src/data/trim_movie_database_Jacob.csv",
                  low_memory=False, index_col=0)
+df = df.reset_index()
 
-# Filter movies to only take the relevant into account.
-# df = df[df["revenue"] > 0]
-# df = df[:5000]
 cSimilarity = init()
-#movies = getRecomendation()
-# printMovies(movies)
